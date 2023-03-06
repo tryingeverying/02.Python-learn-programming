1b32b7a9b3a7df5adbfd06bdb0a950576ef0b93a
“Trying_everything” <mwq1742801515@163.com>
2023-03-06 21:53:06 +0800

    更改位置

--- 3_Python-100-Days-master/day21-30/res/js/hello.js
+++
@@ -1,8 +0,0 @@
-function randomColor(opacity=1) {
-	let r = parseInt(Math.random() * 256)
-	// Math.random() 函数返回一个浮点数，伪随机数在范围从0 到小于1
-	// parseInt(string, radix) 解析一个字符串并返回指定基数的十进制整数，
-	let g = parseInt(Math.random() * 256)
-	let b = parseInt(Math.random() * 256)
-	return `rgba(${r}, ${g}, ${b}, ${opacity})`
-}
